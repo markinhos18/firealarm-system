af41f1eec52cfa1fd2bf8b5239d4b906473e262f
markinhos18 <marcosfelipe@mfwebdeveloper.net.br>
2023-08-18 01:02:59 -0300

    Revert "fix login screen"
    This reverts commit c926d50ad757d7a8beb4dfc03db5a0bdd1ac862f.

--- src/components/FormLogin/styles.ts
+++ src/components/FormLogin/styles.ts
@@ -25,7 +25,7 @@ export const Wrapper = styled.div`
     margin: 70px 0 80px;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
       rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
-    width: 30vw;
+    width: 20vw;
 
     ${media.lessThan('medium')`
     width: 100%;
